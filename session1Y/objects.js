7cfc1198f5999d6e62e70bbf39f5e5b36ab86f70
OIH <OIH@VIA.DK>
2022-09-08 15:03:58 +0200

    Fixed syntax of copied dialog

--- session1Y/objects.js
+++ session1Y/objects.js
@@ -10,19 +10,19 @@ let obj2 = {
 
 console.log(obj === obj2)
 
-> obj.z
+obj.z
 //undefined
-> obj.z.toString()
+obj.z.toString()
 //Uncaught TypeError: Cannot read properties of undefined (reading 'toString')
-> obj.z = 'cat'
+obj.z = 'cat'
 //'cat'
-> obj
-{ x: 100, y: 200, z: 'cat' }
-> obj.z = undefined
-undefined
-> obj
-{ x: 100, y: 200, z: undefined }
-> delete obj.z
-true
-> obj
-{ x: 100, y: 200 }
+obj
+//{ x: 100, y: 200, z: 'cat' }
+obj.z = undefined
+//undefined
+obj
+//{ x: 100, y: 200, z: undefined }
+delete obj.z
+// true
+obj
+// { x: 100, y: 200 }
