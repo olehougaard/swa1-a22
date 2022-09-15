39f793896e2b0f5a1a45931c19319cd0834c52ae
OIH <OIH@VIA.DK>
2022-09-15 10:55:53 +0200

    Added exercise

---
+++ session2/exercise1.2.js
@@ -0,0 +1,23 @@
+function Point(x, y) {
+    const getX = () => x
+    const getY = () => y
+    const moveTo = (_x, _y) => {
+        x = _x
+        y = _y
+    }
+    const toString = `(${x}, ${y})`
+    return { getX, getY, moveTo, toString }
+}
+
+function Circle({center, x, y, radius}) {
+    if (center === undefined) center = Point(x, y)
+    const getCenterX = () => center.getX()
+    const getCenterY = () => center.getY()
+    const getRadius = () => radius
+    const moveCenterTo = (x, y) => center.moveTo(x, y)
+    const toString = `center = ${center.toString()}, radius = ${radius}`
+    return { getCenterX, getCenterY, getRadius, moveCenterTo, toString }
+}
+
+const c1 = Circle({center: Point(20, 30), radius: 100})
+const c2 = Circle({ x: 20, y: 30, radius: 100})
