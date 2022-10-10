814dee9de7e53791c8c189aa4cfde28740e06c01
OIH <OIH@VIA.DK>
2022-10-10 14:56:19 +0200

    session 6

--- session6/functional.ts
+++ session6/functional.ts
@@ -8,7 +8,13 @@ const createPerson = (name: string, age: number) => ({name, age})
 type Company = {
   readonly name: string
   readonly address: string
-  readonly employees: Person[]
+  readonly employees: Readonly<Person[]>
 }
 
+const createCompany = (name: string, address: string, employees: Person[] = []): Company => 
+  ({name, address, employees})
 
+const addEmployee = (c: Company, e: Person): Company => 
+    createCompany(c.name, c.address, [...c.employees, e])
+const removeEmployee = (c: Company, e: Person): Company => 
+    createCompany(c.name, c.address, c.employees.filter(ee => e.name !== ee.name))
--- session6/immutable.ts
+++ session6/immutable.ts
@@ -2,7 +2,7 @@ class Person {
     readonly name: string
     readonly age: number
 
-    constructor(name, age) {
+    constructor(name: string, age: number) {
         this.name = name
         this.age = age
     }
@@ -13,17 +13,24 @@ class Company {
     readonly address: string
     private employees: Person[]
 
-    constructor(name, address, employees = []) {
+    constructor(name: string, address: string, employees: Person[] = []) {
         this.name = name
         this.address = address
-        this.employees = employees
+        this.employees = [...employees]
     }
 
-    addEmployee(employee: Person) { 
+    addEmployee(employee: Person): Company {
+        return new Company(this.name, this.address, [...this.employees, employee])
      }
 
     removeEmployee(employee: Person) {
+        return new Company(this.name, this.address, this.employees.filter(e => e.name !== employee.name))
     }
 
-    getEmployees() { return ? }
+    getEmployees() { return [...this.employees] }
 }
+
+let c = new Company("", "")
+c = c
+    .addEmployee(new Person("", 8))
+    .addEmployee(new Person("asdlkfj", 88))
